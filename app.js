c3aeb21706212ef756ddb8177f6d8b8da8f6d3f9
lmonm <lmonmarche@yahoo.fr>
2017-06-22 21:07:56 +0200

    creating files

---
+++ app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by lmonm on 22/06/2017.
+ */
+angular.module('TestForm', []);
+
+angular.module('TestForm')
+    .controller('TestFormController', ['$scope', function ($scope) {
+
+        }]
+    );
